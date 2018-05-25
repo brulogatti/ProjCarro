ab7e266e1c9000facb5ca66a48e27bfd81ec6ead
brulogatti <bru.logatti@gmail.com>
2018-05-24 21:34:13 -0300

    Proj Carro Versão 1

---
+++ src/br/com/service/CarroService.java
@@ -0,0 +1,39 @@
+package br.com.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.controle.CarroCtrl;
+import br.com.entidade.Carro;
+
+@Path("/carro")
+public class CarroService {
+	
+	@GET
+	@Path("/select")
+	public Response select() {
+		return Response.ok(new CarroCtrl().select(Carro.CONS_CARRO_ALL), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/save")
+	public void save(Carro carro) {
+		new CarroCtrl().insert(carro);
+	}
+	
+	@POST
+	@Path("/update")
+	public void update(Carro carro) {
+		new CarroCtrl().update(carro);
+	}
+	
+	@POST
+	@Path("/delete")
+	public void delete(Carro carro) {
+		new CarroCtrl().delete(carro);
+	}
+
+}
---
+++ src/br/com/teste/Teste.java
@@ -0,0 +1,25 @@
+package br.com.teste;
+
+import br.com.controle.CarroCtrl;
+import br.com.entidade.Carro;
+
+public class Teste {
+	
+	public static void main (String[]args) {
+		
+		/*Carro c = new Carro();
+		
+		c.setModelo("FORD");
+		c.setNome("Ka");
+		
+		CarroCtrl controle = new CarroCtrl();
+		controle.insert(c);
+		*/
+		for(Carro carro : new CarroCtrl().select(Carro.CONS_CARRO_ALL)) {
+			System.out.println("Id: " + carro.getId());
+			System.out.println("Nome: " + carro.getNome());;
+			System.out.println("Modelo: " + carro.getModelo());
+		}
+	}
+
+}
